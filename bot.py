ffe2c07c8528d20cd50f14fb75420d054bdec7dc
bpc-main <71730183+c0lg8@users.noreply.github.com>
2023-11-17 23:36:46 -0700

    Update bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+import time
+import os
+import wmi
+
+f = wmi.WMI()
+
+flag = 0
+
+activity = ''
+
+def checkIfRunning():
+    global activity
+    activity = 'Checking Game Status'
+    for process in f.Win32_Process():
+        if "Cities2.exe" == process.Name:
+            print("CS2 is running")
+            flag = 1
+            break
+
+    if flag == 0:
+        print("CS2 is not running")
+
+while True:
+    activity = 'idle'
+    checkIfRunning()
+    print(activity)
+    time.sleep(1)
+    os.system('cls')
