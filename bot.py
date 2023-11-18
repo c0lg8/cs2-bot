c9a66de9532ef6273ab7f80113fcd7b25af38965
bpc-main <71730183+c0lg8@users.noreply.github.com>
2023-11-18 00:04:24 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,19 +9,19 @@ flag = 0
 activity = ''
 
 def checkIfRunning():
+    global flag
     global activity
     activity = 'Checking Game Status'
     for process in f.Win32_Process():
         if "Cities2.exe" == process.Name:
             print("CS2 is running")
             flag = 1
-
     if flag == 0:
         print("CS2 is not running")
+    time.sleep(1)
+    os.system('cls')
 
 while True:
     activity = 'idle'
     checkIfRunning()
     print(activity)
-    time.sleep(1)
-    os.system('cls')
